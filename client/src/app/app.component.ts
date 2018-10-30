2e443df63af9c92c9961795a6533c4cea574255a
Hung Le <skvn.clew@gmail.com>
2018-10-31 00:55:22 +0700

    add heroku server

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,18 +1,23 @@
 import { Component } from '@angular/core';
 import * as io from 'socket.io-client';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
- export class AppComponent {
-  txtMessage: '';
-  socket : any;
+
+export class AppComponent {
+  txtMessage = '';
+  socket: any;
+  messages: string[] = [];
   constructor() {
-    this.socket = io('http://localhost:3000');
+    this.socket = io('https://chat1812.herokuapp.com');
+    this.socket.on('SERVER_SEND_MESSAGE', message => this.messages.push(message));
   }
+
   sendMessage() {
-    alert(this.txtMessage);
+    this.socket.emit('CLIENT_SEND_MESSAGE', this.txtMessage);
     this.txtMessage = '';
   }
 }
--- server/index.js
+++ server/index.js
@@ -1,3 +1,15 @@
-const io = require('socket.io')(3000);
- io.on('connection', socket => console.log(socket.id));
- console.log('Server started.');
+const express = require('express');
+
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+app.use(express.static('dist'));
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => console.log('Server started!'));
+
+io.on('connection', socket => {
+    socket.on('CLIENT_SEND_MESSAGE', message => {
+        io.emit('SERVER_SEND_MESSAGE', message);
+    });
+});
